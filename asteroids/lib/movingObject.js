c1f3cd4862932313483e64595b7deb9fbe2790c0
john+dean(again) <john+dean@appacademy.io>
2015-06-29 11:31:25 -0400

    Phase one complete for Asteroids.

---
+++ asteroids/lib/movingObject.js
@@ -0,0 +1,31 @@
+function MovingObject (obj) {
+  this.pos = obj.pos;
+  this.vel = obj.vel;
+  this.radius = obj.radius;
+  this.color = obj.color;
+};
+
+
+MovingObject.prototype.draw = function (ctx) {
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+
+  ctx.arc(
+    this.pos[0],
+    this.pos[1],
+    this.radius,
+    0,
+    2*Math.PI,
+    false
+  );
+
+  ctx.fill();
+
+};
+
+MovingObject.prototype.move = function () {
+  var newX = this.pos[0] + this.vel[0];
+  var newY = this.pos[1] + this.vel[1];
+
+  this.pos = [newX, newY];  
+}
